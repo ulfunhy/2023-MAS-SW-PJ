dc95a0d99998224b55ab94c9f920ed062f65114f
phinsso <naso01247@gmail.com>
2023-08-23 18:14:47 +0900

    [feat] 모든 기능 구현

--- src/project/Game.java
+++ src/project/Game.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 public class Game {
@@ -29,6 +30,10 @@ public class Game {
 	JLabel comScoreLbl = new JLabel("0"); // 컴퓨터 점수 라벨
 
 	JFrame PlayFrame = new JFrame("게임 화면");
+	JLabel playerChooseLbl = new JLabel(""); // 플레이어가 무엇을 선택했는지
+	JLabel comChooseLbl = new JLabel(""); // 컴퓨터가 무엇을 선택했는지
+
+	String winner; // 승자
 
 	public Game() {
 		StartFrame(); // 시작화면 불러오기 (프로그램이 시작화면부터 이어짐)
@@ -37,17 +42,21 @@ public class Game {
 	public void StartFrame() { // 시작 화면 메소드
 
 		JButton StartBtn = new JButton(); // 게임시작 버튼
-		JButton ExplainBtn = new JButton("게임 설명"); // 게임설명 버튼
+		JButton ExplainBtn = new JButton(); // 게임설명 버튼
 
-		StartBtn.setBounds(80, 560, 217, 70);
+		StartBtn.setBounds(80, 560, 250, 80);
+		// StartBtn.setOpaque(false);
 		StartFrame.add(StartBtn);
-		ExplainBtn.setBounds(80, 470, 217, 70);
+		ExplainBtn.setBounds(80, 470, 250, 80);
 		StartFrame.add(ExplainBtn);
 
-		StartFrame.setLayout(null); // 시작 화면의 배치 방법을 null로 지정
+		// StartFrame.setLayout(null); // 시작 화면의 배치 방법을 null로 지정
 
 		ImagePanel StartBtnImg = new ImagePanel(new ImageIcon("./image/게임시작버튼.png").getImage());
 		StartBtn.add(StartBtnImg);
+
+		ImagePanel ExplainBtnImg = new ImagePanel(new ImageIcon("./image/게임설명버튼.png").getImage());
+		ExplainBtn.add(ExplainBtnImg);
 
 		ImagePanel StartFrameImg = new ImagePanel(new ImageIcon("./image/게임시작화면.jpg").getImage());
 		StartFrame.add(StartFrameImg);
@@ -74,6 +83,29 @@ public class Game {
 		StartFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫았을 때 프로그램이 종료되게 함
 	}
 
+	ActionListener btnListener = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			for (int i = 0; i < SRPbtn.length; i++) {
+				if (e.getSource() == SRPbtn[i]) {
+					// 해당 버튼에 따라 playerChoose에 값을 할당
+					playerChoose = i;
+					System.out.println("Player 선택: " + playerChoose);
+
+					if (playerChoose == 0) {
+						playerChooseLbl.setText("가위");
+					} else if (playerChoose == 1) {
+						playerChooseLbl.setText("바위");
+					} else if (playerChoose == 2) {
+						playerChooseLbl.setText("보");
+					}
+
+				}
+			}
+		}
+	};
+
 	public void PlayFrame() { // 게임진행 화면
 
 		for (int i = 0; i < SRPbtn.length; i++) {
@@ -99,7 +131,15 @@ public class Game {
 		timerLbl.setFont(new Font("고딕", Font.BOLD, 35));
 		PlayFrame.add(timerLbl);
 
-		ImagePanel PlayFrameImg = new ImagePanel(new ImageIcon("./image/게임진행화면.jpg").getImage());
+		playerChooseLbl.setBounds(540, 250, 140, 140);
+		playerChooseLbl.setFont(new Font("고딕", Font.BOLD, 40));
+		PlayFrame.add(playerChooseLbl);
+
+		comChooseLbl.setBounds(220, 250, 140, 140);
+		comChooseLbl.setFont(new Font("고딕", Font.BOLD, 40));
+		PlayFrame.add(comChooseLbl);
+
+		ImagePanel PlayFrameImg = new ImagePanel(new ImageIcon("./image/게임진행화면.png").getImage());
 		PlayFrame.add(PlayFrameImg);
 
 		// 게임이 3초마다 진행되게 하는 타이머
@@ -114,21 +154,28 @@ public class Game {
 				if (secondsPassed == 0) {
 					PlayLogic();
 					System.out.println("게임 카운트: " + gameCount);
+					System.out.println("======================");
 
 					if (gameCount >= 3) {
 						((Timer) evt.getSource()).stop();
 						// 게임 종료 후 승자 결정
-						String winner;
 						if (playerScore > comScore) {
-							winner = "플레이어";
+							winner = "player";
 						} else if (comScore > playerScore) {
-							winner = "컴퓨터";
+							winner = "com";
 						} else {
-							winner = "무승부";
+							winner = "both";
 						}
 						System.out.println("게임 종료! " + winner + " 승리!");
-						PlayFrame.setVisible(false);
-						ResultFrame();
+
+						try {
+							Thread.sleep(3000);
+							PlayFrame.setVisible(false);
+							ResultFrame();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+
 					}
 
 					secondsPassed = 3;
@@ -145,25 +192,39 @@ public class Game {
 		PlayFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	ActionListener btnListener = new ActionListener() {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			for (int i = 0; i < SRPbtn.length; i++) {
-				if (e.getSource() == SRPbtn[i]) {
-					// 해당 버튼에 따라 playerChoose에 값을 할당
-					playerChoose = i;
-					System.out.println("Player 선택: " + playerChoose);
-				}
-			}
-		}
-	};
-
 	public void PlayLogic() {
+		/*
+		 * comChooseLbl.setText(""); playerChooseLbl.setText("");
+		 */
+
 		Random comRd = new Random();
 		comChoose = comRd.nextInt(3);
-		System.out.println("컴퓨터 선택: " + comChoose);
 
+		/*
+		 * if(comChoose == 0) comChooseLbl.setText("가위"); else if(comChoose == 1)
+		 * comChooseLbl.setText("바위"); else comChooseLbl.setText("보");
+		 * 
+		 * System.out.println("컴퓨터 선택: " + comChoose);
+		 * 
+		 * switch (playerChoose - comChoose) { case 0: System.out.println("비겼습니다.");
+		 * break; case 1: case -2: System.out.println("이겼습니다."); playerScore++; break;
+		 * case 2: case -1: System.out.println("졌습니다."); comScore++; break; }
+		 */
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if (comChoose == 0)
+					comChooseLbl.setText("가위");
+				else if (comChoose == 1)
+					comChooseLbl.setText("바위");
+				else
+					comChooseLbl.setText("보");
+
+				System.out.println("컴퓨터 선택: " + comChoose);
+			}
+		});
+		
 		switch (playerChoose - comChoose) {
 		case 0:
 			System.out.println("비겼습니다.");
@@ -182,6 +243,20 @@ public class Game {
 
 		System.out.println("플레이어 점수: " + playerScore + " vs " + "컴퓨터 점수: " + comScore);
 
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				playerScoreLbl.setText(Integer.toString(playerScore)); // 게임 중 플레이어 점수
+			}
+		});
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				comScoreLbl.setText(Integer.toString(comScore)); // 게임 중 컴퓨터 점수
+			}
+		});
+
 		playerChoose = -1;
 		gameCount++;
 	}
@@ -189,22 +264,32 @@ public class Game {
 	public void ResultFrame() {
 		JFrame ResultFrame = new JFrame("결과 화면");
 
-		ImagePanel ResultFrameImg = new ImagePanel(new ImageIcon("./image/게임결과화면.jpg").getImage());
-		ResultFrame.add(ResultFrameImg);
+		String winnerFrameName;
 
-		ResultFrame.setVisible(true);
-		ResultFrame.setResizable(false);
-		ResultFrame.setSize(840, 720);
-		ResultFrame.setLocationRelativeTo(null);
-		ResultFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		if (winner.equals("player"))
+			winnerFrameName = "./image/게임결과화면-P.png";
+		else if (winner.equals("com"))
+			winnerFrameName = "./image/게임결과화면-C.png";
+		else
+			winnerFrameName = "./image/게임결과화면-D.png";
 
-		JButton AgainBtn = new JButton("게임다시시작"); // 게임설명 버튼
-		JButton EndBtn = new JButton("게임종료"); // 게임시작 버튼
+		JLabel comResultScoreLbl = new JLabel(Integer.toString(comScore)); // com 최종 점수
+		comResultScoreLbl.setBounds(240, 310, 230, 230);
+		comResultScoreLbl.setFont(new Font("고딕", Font.BOLD, 60));
+		ResultFrame.add(comResultScoreLbl);
 
-		AgainBtn.setBounds(158, 522, 217, 70);
+		JLabel playerResultScoreLbl = new JLabel(Integer.toString(playerScore)); // player 최종 점수
+		playerResultScoreLbl.setBounds(540, 310, 230, 230);
+		playerResultScoreLbl.setFont(new Font("고딕", Font.BOLD, 60));
+		ResultFrame.add(playerResultScoreLbl);
+
+		JButton AgainBtn = new JButton(); // 게임설명 버튼
+		JButton EndBtn = new JButton(); // 게임시작 버튼
+
+		AgainBtn.setBounds(158, 522, 227, 80);
 		ResultFrame.add(AgainBtn);
 
-		EndBtn.setBounds(465, 522, 217, 70);
+		EndBtn.setBounds(465, 522, 227, 80);
 		ResultFrame.add(EndBtn);
 
 		// 버튼 배경 이미지 설정
@@ -227,12 +312,15 @@ public class Game {
 			}
 		});
 
+		ImagePanel ResultFrameImg = new ImagePanel(new ImageIcon(winnerFrameName).getImage());
+		ResultFrame.add(ResultFrameImg);
+
 		ResultFrame.setLayout(null);
 		ResultFrame.setVisible(true);
 		ResultFrame.setResizable(false);
 		ResultFrame.setSize(840, 720);
 		ResultFrame.setLocationRelativeTo(null);
-		ResultFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 창을 닫았을 때 해당 창만 종료
+		ResultFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public void ExplainFrame() { // 게임설명 화면
